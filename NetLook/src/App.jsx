9e519556770030959b081bf30134b5f0edbc1be4
YashwanthKumar02 <yashwanthnani2@gmail.com>
2024-05-10 03:29:03 +0530

    setup environment variables and conf file

--- NetLook/src/App.jsx
+++ NetLook/src/App.jsx
@@ -1,6 +1,11 @@
-import { useState } from 'react'
+import React from 'react'
+import conf from '../conf/conf';
 
 function App() {
+
+  //console.log(process.env.REACT_APP_APPWRITE_ENDPOINT);
+  console.log(conf.appwriteEndpoint);
+
 
   return (
     <>
